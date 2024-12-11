0f597c4c61f93f60bc2be424dd5b98d20b66698a
Dainerdev <florianarroyo15@gmail.com>
2024-12-11 16:22:49 -0500

    Vista de la pantalla para buscar gastos

---
+++ lib/ui/expenseScreens/expense.search.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_und4/ui/expenseScreens/expense.dart';
+
+class SearchExpenseScreen extends StatefulWidget {
+  const SearchExpenseScreen({super.key});
+
+  @override
+  State<SearchExpenseScreen> createState() => _SearchEarningScreenState();
+  
+}
+
+class _SearchEarningScreenState extends State<SearchExpenseScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 100.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Buscar Gasto',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 141, 74, 180),
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+
+              const SizedBox(height: 60),
+
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Nombre',
+                  border: OutlineInputBorder()
+                ),
+              ),
+
+              const SizedBox(height: 40),
+
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: (){
+                        Navigator.push(context, 
+                          MaterialPageRoute(builder: (context) => ExpensesScreen())
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 245, 230, 253),
+                        padding: const EdgeInsets.symmetric(horizontal: 45, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)
+                        )
+                      ),
+                      child: const Text(
+                        'Regresar',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 141, 74, 180),
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    )
+                  ),
+
+                  const SizedBox(width: 10),
+
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: (){
+
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 141, 74, 180),
+                        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)
+                        )
+                      ),
+                      label: const Text(
+                        'Buscar',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      icon: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                        size: 24,
+                      ),
+                    )
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
