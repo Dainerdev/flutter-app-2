9cd7480a2e35dbfc467e9b5c448ec8cd29a1b9cb
Dainerdev <florianarroyo15@gmail.com>
2024-12-11 18:28:26 -0500

    Funcion para obtener los datos del gasto y modificarlos

--- lib/ui/expenseScreens/expense.edit.dart
+++ lib/ui/expenseScreens/expense.edit.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: use_build_context_synchronously
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app_und4/models/expense.model.dart';
-import 'package:flutter_app_und4/ui/expenseScreens/expense.search.dart';
+import 'package:http/http.dart' as http;
 
 class EditExpenseScreen extends StatefulWidget {
 
@@ -18,7 +21,97 @@ class _EditExpenseScreenState extends State<EditExpenseScreen> {
   late TextEditingController nameField;
   late TextEditingController amountField;
   late TextEditingController descriptionField;
-  
+
+  // obtener los datos del gasto
+  @override
+  void initState() {
+    super.initState();
+    nameField = TextEditingController(text: widget.expense.name);
+    amountField = TextEditingController(text: widget.expense.amount.toString());
+    descriptionField = TextEditingController(text: widget.expense.description);
+  }
+
+  @override
+  void dispose() {
+    nameField.dispose();
+    amountField.dispose();
+    descriptionField.dispose();
+    super.dispose();
+  }
+
+  void _saveExpense() async {
+    final updatedExpense = Expense(
+      id: widget.expense.id, 
+      name: nameField.text,
+      amount: double.parse(amountField.text),
+      description: descriptionField.text
+    );
+
+    // llama a la API para actualizar el gasto en la BD
+    final response = await _updateExpense(updatedExpense);
+    if (response.statusCode == 200) {      
+      Navigator.pop(context, updatedExpense);
+      showDialog(
+        context: context,
+        barrierDismissible: false,                      
+        barrierColor: Color.fromARGB(180, 0, 0, 0),          
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Notificación!',
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  Text('Gasto modificado exitosamente.',
+                    style: TextStyle(
+                      fontSize: 16
+                    ),
+                  )
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: (){
+                  Navigator.of(context).pop();
+                }, 
+                child: Text('Continuar',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              )
+            ],
+          );
+        }
+      );
+    } else {
+      // Muestra un mensaje de error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al actualizar el gasto')),
+      );
+    }
+  }
+
+  // Enviar los datos nuevos a la API
+  Future<http.Response> _updateExpense(Expense expense) async {
+    final apiUrl = Uri.parse('http://10.0.2.2:3312/api/expenses/edit/${expense.id}');
+    final response = await http.put(
+      apiUrl,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'name': expense.name,
+        'description': expense.description,
+        'amount': expense.amount,
+      }),
+    );
+    return response;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,7 +164,7 @@ class _EditExpenseScreenState extends State<EditExpenseScreen> {
 
               ElevatedButton(
                 onPressed: (){
-
+                  _saveExpense();
                 }, 
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color.fromARGB(255, 141, 74, 180),
@@ -95,9 +188,7 @@ class _EditExpenseScreenState extends State<EditExpenseScreen> {
 
               ElevatedButton(
                 onPressed: (){
-                  Navigator.push(context, 
-                    MaterialPageRoute(builder: (context) => SearchExpenseScreen())
-                  );
+                  Navigator.pop(context);
                 }, 
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color.fromARGB(255, 245, 230, 253),
