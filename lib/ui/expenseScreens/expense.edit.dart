65be863fd34ac9243f96e9c0e0b25c52aa4e3ec1
Dainerdev <florianarroyo15@gmail.com>
2024-12-11 17:26:29 -0500

    Vista para pantalla de modificacion de gastos

---
+++ lib/ui/expenseScreens/expense.edit.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_und4/models/expense.model.dart';
+import 'package:flutter_app_und4/ui/expenseScreens/expense.search.dart';
+
+class EditExpenseScreen extends StatefulWidget {
+
+  final Expense expense;
+
+  const EditExpenseScreen({super.key,  required this.expense});
+
+  @override
+  State<EditExpenseScreen> createState() => _EditExpenseScreenState();
+  
+}
+
+class _EditExpenseScreenState extends State<EditExpenseScreen> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Modificar Gasto',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 141, 74, 180),
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+
+              const SizedBox(height: 50),
+
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Nombre',
+                  border: OutlineInputBorder()
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Monto',
+                  border: OutlineInputBorder()
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Descripción',
+                  border: OutlineInputBorder()
+                ),
+              ),
+
+              const SizedBox(height: 60),
+
+              ElevatedButton(
+                onPressed: (){
+
+                }, 
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 141, 74, 180),
+                  padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)
+                  )
+                ),
+                child: const Text(
+                  'Editar',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              ),
+
+              const SizedBox(height: 20),
+
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.push(context, 
+                    MaterialPageRoute(builder: (context) => SearchExpenseScreen())
+                  );
+                }, 
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 245, 230, 253),
+                  padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)
+                  )
+                ),
+                child: const Text(
+                  'Regresar',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 141, 74, 180),
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
