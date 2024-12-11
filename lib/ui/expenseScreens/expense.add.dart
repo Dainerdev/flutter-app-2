a459ca9fa43b4242b1d5a7fc7638966d3c013a54
Dainerdev <florianarroyo15@gmail.com>
2024-12-11 15:25:51 -0500

    Vista de la pantalla para agregar gastos

---
+++ lib/ui/expenseScreens/expense.add.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_und4/ui/expenseScreens/expense.dart';
+
+class AddExpenseScreen extends StatefulWidget {
+  const AddExpenseScreen({super.key});
+
+  @override
+  State<AddExpenseScreen> createState() => _AddEarningScreenState();
+  
+}
+
+class _AddEarningScreenState extends State<AddExpenseScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Agregar Gasto',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 141, 74, 180),
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+
+              const SizedBox(height: 50),
+
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Nombre',
+                  border: OutlineInputBorder()
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Monto',
+                  border: OutlineInputBorder()
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Descripción',
+                  border: OutlineInputBorder()
+                ),
+              ),
+
+              const SizedBox(height: 60),
+
+              ElevatedButton(
+                onPressed: (){
+
+                }, 
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 141, 74, 180),
+                  padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)
+                  )
+                ),
+                child: const Text(
+                  'Guardar',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              ),
+
+              const SizedBox(height: 20),
+
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.push(context, 
+                    MaterialPageRoute(builder: (context) => ExpensesScreen())
+                  );
+                }, 
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 245, 230, 253),
+                  padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)
+                  )
+                ),
+                child: const Text(
+                  'Regresar',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 141, 74, 180),
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
