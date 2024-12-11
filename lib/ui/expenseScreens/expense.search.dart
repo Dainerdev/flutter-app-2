88cdb76c606fc9bab490f1c2348554607545aa89
Dainerdev <florianarroyo15@gmail.com>
2024-12-11 16:38:01 -0500

    Metodo para obtener gastos por nombre

--- lib/ui/expenseScreens/expense.search.dart
+++ lib/ui/expenseScreens/expense.search.dart
@@ -1,23 +1,29 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app_und4/models/expense.model.dart';
 import 'package:flutter_app_und4/ui/expenseScreens/expense.dart';
 import 'package:flutter_app_und4/ui/login.dart';
+import 'package:http/http.dart' as http;
 
 class SearchExpenseScreen extends StatefulWidget {
   const SearchExpenseScreen({super.key});
 
   @override
-  State<SearchExpenseScreen> createState() => _SearchEarningScreenState();
+  State<SearchExpenseScreen> createState() => _SearchExpenseScreenState();
   
 }
 
-class _SearchEarningScreenState extends State<SearchExpenseScreen> {
+class _SearchExpenseScreenState extends State<SearchExpenseScreen> {
   
   // identificadores
   final nameField = TextEditingController();
 
-  // Lista de ingresos
-  List<Expense> earnings = [];
+  // Lista de gastos
+  List<Expense> expenses = [];
+
   String? userId;
   Map<String, dynamic>? userData;
 
@@ -36,6 +42,80 @@ class _SearchEarningScreenState extends State<SearchExpenseScreen> {
     setState(() {
       userId = id;
     });
+  }
+  
+  // Metodo para buscar gastos por nombre
+  Future<void> _searchExpensesByName(String name) async {
+
+    final user = userId!;
+    
+    final apiUrl = Uri.parse('http://10.0.2.2:3312/api/expenses/name/$user/$name'); 
+
+    try {
+      final response = await http.get(apiUrl);
+
+      if (response.statusCode == 200) {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          barrierColor: Color.fromARGB(180, 0, 0, 0), 
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text(
+                'Notifiación!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: [
+                    Text(
+                      'Se encontró almenos un gasto con el nombre indicado.',
+                      style: TextStyle(
+                        fontSize: 16
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: (){
+                    Navigator.of(context).pop();
+                  }, 
+                  child: Text(
+                    'Entiendo',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold
+                    ),
+                  )
+                ),
+              ],
+            );
+          }
+        );
+        
+        // Decodificar la respuesta en formato JSON
+        List<dynamic> jsonData = json.decode(response.body);
+
+        // Convertir la lista de JSON a una lista de objetos Expenses
+        setState(() {
+          expenses = jsonData.map((data) => Expense.fromJson(data)).toList();
+        });
+
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('No se encontraron gastos con ese nombre.'))
+        );
+      }
+
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e'))
+      );     
+    }
   }
 
   @override
@@ -102,6 +182,52 @@ class _SearchEarningScreenState extends State<SearchExpenseScreen> {
                     child: ElevatedButton.icon(
                       onPressed: (){
 
+                        if (nameField.text == '') {
+                          showDialog(
+                            context: context,
+                            barrierDismissible: false,
+                            barrierColor: Color.fromARGB(180, 0, 0, 0),
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('Notificación!',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                                content: SingleChildScrollView(
+                                  child: ListBody(
+                                    children: [
+                                      Text(
+                                        'Indique el nombre del gasto a buscar.',
+                                        style: TextStyle(
+                                          fontSize: 16
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: (){
+                                      Navigator.of(context).pop();
+                                    }, 
+                                    child: Text(
+                                      'Entiendo',
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold
+                                      ),
+                                    )
+                                  )
+                                ],
+                              );
+                            }
+                          );
+
+                        } else {
+                          _searchExpensesByName(nameField.text);
+                        }
+
                       },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Color.fromARGB(255, 141, 74, 180),
@@ -126,6 +252,43 @@ class _SearchEarningScreenState extends State<SearchExpenseScreen> {
                     )
                   )
                 ],
+              ),
+
+              const SizedBox(height: 50),
+
+              Text('Información del Gasto:',
+                style: TextStyle(
+                  fontSize: 20
+                ),
+              ),
+
+              // Informacion del Gasto
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: expenses.length,
+                itemBuilder: (context, index) {
+                  final expense = expenses[index];
+                  return Card(
+                    color: Color.fromARGB(255, 245, 230, 253),
+                    elevation: 6,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    child: ListTile(
+                      title: Text('Monto: \$${expense.amount.toStringAsFixed(2)}',
+                      style: TextStyle(
+                          fontSize: 22,
+                          color: Color.fromARGB(255, 141, 74, 180),
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      subtitle: Text('Nombre: ${expense.name}\nDescripción: ${expense.description}',
+                        style: TextStyle(
+                          fontSize: 18
+                        ),
+                      ),
+                      isThreeLine: true,
+                    ),
+                  );
+                },
               ),
             ],
           ),
