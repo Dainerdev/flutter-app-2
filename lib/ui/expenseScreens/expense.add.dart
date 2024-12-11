08feac671163decd331d500fd071e38a14ced9e2
Dainerdev <florianarroyo15@gmail.com>
2024-12-11 16:06:26 -0500

    Se agrega metodo para guardar gastos

--- lib/ui/expenseScreens/expense.add.dart
+++ lib/ui/expenseScreens/expense.add.dart
@@ -1,6 +1,11 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app_und4/ui/expenseScreens/expense.dart';
 import 'package:flutter_app_und4/ui/login.dart';
+import 'package:http/http.dart' as http;
 
 class AddExpenseScreen extends StatefulWidget {
   const AddExpenseScreen({super.key});
@@ -42,7 +47,101 @@ class _AddEarningScreenState extends State<AddExpenseScreen> {
     });
   }
 
+  // funcion para comunicar con la api y guardar gasto
+  Future<void> save() async {
 
+    final apiUrl = Uri.parse('http://10.0.2.2:3312/api/expenses/');
+
+    //Mapa de los datos del usuario
+    final Map<String, dynamic> expenseData = {
+
+      "user_id": userId!,
+      "name": nameField.text,
+      "amount": amountField.text,
+      "description": descriptionField.text
+
+    };
+
+    try {
+      
+      // Solicitud POST a la API
+      final response = await http.post(apiUrl,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(expenseData)
+      );
+
+      if (response.statusCode == 200) {
+        showDialog(
+          context: context,
+          barrierDismissible: false,                      
+          barrierColor: Color.fromARGB(180, 0, 0, 0),          
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Notificación!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: [
+                    Text('Ingreso guardado exitosamente.',
+                      style: TextStyle(
+                        fontSize: 16
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: (){
+                    Navigator.of(context).pop();
+                    Navigator.push(context, 
+                      MaterialPageRoute(builder: (context) => ExpensesScreen())
+                    );
+                  }, 
+                  child: Text('Continuar',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold
+                    ),
+                  )
+                )
+              ],
+            );
+          }
+        );
+
+      } else {
+        showDialog(
+          context: context, 
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Error'),
+              content: Text('No se pudo guardar el ingreso. Inténtelo neuvamente.'),
+              actions: [
+                TextButton(
+                  onPressed: (){
+                    Navigator.of(context).pop();
+                  }, 
+                  child: Text('Entiendo',
+                    style: TextStyle(
+                      fontSize: 16
+                    ),
+                  )
+                )
+              ],
+            );
+          }
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e'))
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -142,9 +241,9 @@ class _AddEarningScreenState extends State<AddExpenseScreen> {
                         );
                       }
                     );
-
+                  } else {
+                    save();
                   }
-
                 }, 
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color.fromARGB(255, 141, 74, 180),
