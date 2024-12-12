1b19ef454b63a3df12faf98496bcf3bae7d6bfd0
Dainerdev <florianarroyo15@gmail.com>
2024-12-11 19:28:27 -0500

    Metodo aplicado para editar los ingresos

--- lib/ui/earningScreens/earning.edit.dart
+++ lib/ui/earningScreens/earning.edit.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app_und4/models/earning.model.dart';
+import 'package:http/http.dart' as http;
 
 class EditEarningScreen extends StatefulWidget {
 
@@ -15,6 +20,96 @@ class _EditEarningScreenState extends State<EditEarningScreen> {
   late TextEditingController nameField;
   late TextEditingController amountField;
   late TextEditingController descriptionField;
+
+  // obtener los datos del ingreso
+  @override
+  void initState() {
+    super.initState();
+    nameField = TextEditingController(text: widget.earning.name);
+    amountField = TextEditingController(text: widget.earning.amount.toString());
+    descriptionField = TextEditingController(text: widget.earning.description);
+  }
+
+  @override
+  void dispose() {
+    nameField.dispose();
+    amountField.dispose();
+    descriptionField.dispose();
+    super.dispose();
+  }
+
+  void _saveEarning() async {
+    final updatedEarning = Earning(
+      id: widget.earning.id, 
+      name: nameField.text,
+      amount: double.parse(amountField.text),
+      description: descriptionField.text
+    );
+
+    // llama a la API para actualizar el ingreso en la BD
+    final response = await _updatedEarning(updatedEarning);
+    if (response.statusCode == 200) {      
+      Navigator.pop(context, updatedEarning);
+      showDialog(
+        context: context,
+        barrierDismissible: false,                      
+        barrierColor: Color.fromARGB(180, 0, 0, 0),          
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Notificación!',
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  Text('Ingreso modificado exitosamente.',
+                    style: TextStyle(
+                      fontSize: 16
+                    ),
+                  )
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: (){
+                  Navigator.of(context).pop();
+                }, 
+                child: Text('Continuar',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              )
+            ],
+          );
+        }
+      );
+    } else {
+      // Muestra un mensaje de error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al actualizar el ingreso')),
+      );
+    }
+  }
+
+    // Enviar los datos nuevos a la API
+  Future<http.Response> _updatedEarning(Earning earning) async {
+    final apiUrl = Uri.parse('http://10.0.2.2:3312/api/earnings/edit/${earning.id}');
+    final response = await http.put(
+      apiUrl,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'name': earning.name,
+        'description': earning.description,
+        'amount': earning.amount,
+      }),
+    );
+    return response;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,7 +164,7 @@ class _EditEarningScreenState extends State<EditEarningScreen> {
 
               ElevatedButton(
                 onPressed: (){
-
+                  _saveEarning();
                 }, 
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color.fromARGB(255, 141, 74, 180),
